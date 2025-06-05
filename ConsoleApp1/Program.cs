dcd1fc10f78e031c07e2376596b53163fc3935ac
Григорій . <mcwertyge@gmail.com>
2025-06-05 21:16:01 +0300

    add some details

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Why do you even read this?");
+Console.WriteLine("Ah, bite me, ok, you deserve this:");
+Console.WriteLine("         ,MMM8&&&.\n    _...MMMMM88&&&&..._\n .::'''MMMMM88&&&&&&'''::.\n::     MMMMM88&&&&&&     ::\n'::....MMMMM88&&&&&&....::'\n   `''''MMMMM88&&&&''''`\n   jgs   'MMM8&&&'");
+//\(@^0^@)/
